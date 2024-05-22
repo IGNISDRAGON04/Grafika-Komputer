71c735ebef75017bdf3d1560e14b6e24dcfb7eef
Farrel G <farrel.gilang04@gmail.com>
2024-05-22 10:33:53 +0700

    Projek Game Grafkom

---
+++ projek_grafkomFix/projek_grafkomFix/start.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class start here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class start extends World
+{
+    /**
+     * Constructor for objects of class start.
+     * 
+     */
+    public start()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(300, 500, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        start_button start_button = new start_button();
+        addObject(start_button,153,238);
+    }
+}
